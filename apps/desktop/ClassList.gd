e84552aa84a0a3a21ef31935e6b3cf7d9bff1475
andrew-wilkes <github@urgentclick.com>
2021-11-25 14:06:09 +0000

    Add weighted list sorting

--- apps/desktop/ClassList.gd
+++ apps/desktop/ClassList.gd
@@ -5,33 +5,49 @@ var list: VBoxContainer
 func _ready():
 	list = $VBox/C/VBox
 	for class_item in Data.settings.class_list:
-		var label = Label.new()
-		label.text = class_item.keyword
-		list.add_child(label)
+		var button = Button.new()
+		button.align = Button.ALIGN_LEFT
+		button.flat = true
+		button.text = class_item.keyword
+		list.add_child(button)
+		button.connect("pressed", self, "item_pressed", [button])
 	update_labels()
 	$VBox/SS.grab_focus()
 
 
+func item_pressed(button):
+	for class_item in Data.settings.class_list:
+		if class_item.keyword == button.text:
+			class_item.weight += 1
+	update_labels()
+
+
 func update_labels():
 	var weights = []
-	var weighted_items = {}
+	var weighted_items = []
 	var unweighted_items = []
 	for class_item in Data.settings.class_list:
 		var weight = class_item.weight
 		if weight > 0:
 			weights.append(weight)
-			weighted_items[weight] = class_item.keyword
+			weighted_items.append(class_item.duplicate())
 		else:
 			unweighted_items.append(class_item.keyword)
 	weights.sort()
 	weights.invert()
 	var i = 0
 	for weight in weights:
-		list.get_child(i).text = weighted_items[weight]
+		for item in weighted_items: # Find first item with this weight
+			if item.weight == weight:
+				list.get_child(i).text = item.keyword
+				item.weight = 0 # Skip this item for this weight from now on
+				break
 		i += 1
 	for keyword in unweighted_items:
 		list.get_child(i).text = keyword
 		i += 1
+	for item in list.get_children():
+		item.visible = true
 
 
 func _on_SS_text_changed(new_text: String):
